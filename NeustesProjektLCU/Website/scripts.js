c9e91902f738666fb05614af5638752e482f69c8
Lucas.Fya <107124564+LucasNicolasSchulz@users.noreply.github.com>
2023-09-18 19:24:01 +0200

    Add files via upload

---
+++ NeustesProjektLCU/Website/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('greetBtn').addEventListener('click', function() {
+    let greetingDiv = document.getElementById('greeting');
+    greetingDiv.textContent = "Hallo, schön dich zu sehen!";
+});
